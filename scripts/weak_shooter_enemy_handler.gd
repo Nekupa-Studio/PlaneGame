5598465de57ef7670ddcbfe54aba1045ac05f26b
VoidScientist <louthosi59@gmail.com>
2023-05-29 00:20:26 +0200

    Enemies move
    They do move, and even go back to try and kill ya.
    
    Tho, it's ugly, so either we need to rework the textures (just another one for when the planes go in the opposite direction) or just change the system
    
    Tbh, the game "board" is a bit too small, so even tho the planes seem slow af, they actually go thru the map quite quickly

--- scripts/weak_shooter_enemy_handler.gd
+++ scripts/weak_shooter_enemy_handler.gd
@@ -1,11 +1,26 @@
 extends Planes
 class_name Enemy
 
+var speed: int = 60
+var direction := Vector2(0,1)
+
 @onready var sprite = $sprite
 	
 func handle_hit(col):
 	if col is Bullet or col is Player:
 		crash_plane()
+
+func _physics_process(delta):
+	super(delta)
+	velocity = direction * speed
+	move_and_slide()
+	
+	print(position.y)
+	
+	if position.y > 550 or position.y < 0:
+		sprite.flip_v = !sprite.flip_v
+		direction = -direction
+
 
 func _get_class():
 	return Enemy
