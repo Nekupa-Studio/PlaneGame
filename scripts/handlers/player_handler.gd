8f822208321f27ff6797a7f25113a0002f1bfed2
VoidScientist <louthosi59@gmail.com>
2025-05-01 15:08:59 +0200

    added condition for debug key
    Now it shouldn't find its way into prod, or at least it wouldn't work.

--- scripts/handlers/player_handler.gd
+++ scripts/handlers/player_handler.gd
@@ -29,7 +29,7 @@ func _physics_process(_delta: float) -> void:
 	move()
 	
 	# TODO: this is debug. remove in prod lmao.
-	if Input.is_action_just_pressed("DEBUG_RELOAD"): 
+	if Input.is_action_just_pressed("DEBUG_RELOAD") and OS.is_debug_build(): 
 		get_tree().reload_current_scene()
 	
 	if Input.is_action_pressed("shoot"):
